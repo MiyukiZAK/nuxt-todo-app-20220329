22acc2706cdd0f92dd4cd8e5779642cf6bd8d655
Miyuki_ZAK <miyukizozaki@gmail.com>
2022-03-29 06:06:57 +0900

    first commit

---
+++ pages/fun.vue
@@ -0,0 +1,6 @@
+<template>
+  <main>
+    <h2>This page is for fun!</h2>
+    <NuxtLink to="/">Back to the home page</NuxtLink>
+  </main>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <main>
+    <h1>Todo App</h1>
+    <p>Create a list of tasks</p>
+
+    <div class="create-new">
+      <input 
+      type="text" 
+      v-model="newTask" 
+      placeholder="Add a new task" 
+      @keypress.enter="addTask" />
+      <button @click="addTask">Add</button>
+    </div>
+
+    <div class="tasks">
+
+    </div>
+
+
+  </main>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      newTask: ''
+    }
+  },
+  methods: {
+    addTask () {
+      if (this.newTask){
+        this.$store.commit('ADD_TASK', this.newTask);
+        this.newTask = '';
+      }
+    }
+  }
+}
+</script>
